074ea01016a039d9b4d1ddbb9bf7b2e8b3e691d8
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2025-08-28 19:47:33 +0100

    Update settings.py

--- Backend/gestion/settings.py
+++ Backend/gestion/settings.py
@@ -88,25 +88,21 @@ TEMPLATES = [
     },
 ]
 
-if os.getenv('MYSQL_URL'):
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ['MYSQL_URL'], engine='django.db.backends.mysql')
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv('MYSQLDATABASE', 'gestion'),
-            'USER': os.getenv('MYSQLUSER', 'root'),
-            'PASSWORD': os.getenv('MYSQLPASSWORD', ''),
-            'HOST': os.getenv('MYSQLHOST', 'mysql.railway.internal'),
-            'PORT': os.getenv('MYSQLPORT', '3306'),
-            'OPTIONS': {
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-                'charset': 'utf8mb4',
-            },
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE') or os.environ.get('MYSQLDATABASE') or config('DB_NAME'),
+        'USER': os.environ.get('MYSQL_USER') or os.environ.get('MYSQLUSER') or config('DB_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD') or os.environ.get('MYSQLPASSWORD') or config('DB_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST') or os.environ.get('MYSQLHOST') or config('DB_HOST'),
+        'PORT': os.environ.get('MYSQL_PORT') or os.environ.get('MYSQLPORT') or config('DB_PORT', default='3306'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
+}
+
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
