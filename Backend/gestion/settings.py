2dd7995a3e2f95a03c8ff82d7e01f13255a9063a
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2025-08-28 18:13:02 +0100

    Update settings.py

--- Backend/gestion/settings.py
+++ Backend/gestion/settings.py
@@ -92,15 +92,17 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE'),
-        'USER': os.environ.get('MYSQLUSER'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-        'HOST': os.environ.get('MYSQLHOST'),
+        'NAME': os.environ.get('MYSQLDATABASE', 'gestion'),
+        'USER': os.environ.get('MYSQLUSER', 'root'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD', 'MhobbifualRHOndCHTXouMDQKlWewwbA'),
+        'HOST': os.environ.get('MYSQLHOST', 'mysql.railway.internal'),
         'PORT': os.environ.get('MYSQLPORT', '3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
+            'connect_timeout': 10,  # 10 second timeout
         },
+        'CONN_MAX_AGE': 300,  # 5 minutes
     }
 }
 
